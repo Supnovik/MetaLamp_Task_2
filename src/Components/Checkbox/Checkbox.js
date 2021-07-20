29038e27fbc2025599a1233e315f523da92aa32f
Supnovik <nikitka.malyutin52@gmail.com>
2021-07-21 00:56:47 +0700

    checkbox complete

--- src/Components/Checkbox/Checkbox.js
+++ src/Components/Checkbox/Checkbox.js
@@ -3,33 +3,50 @@ import "./Checkbox.scss";
 
 function Checkbox() {
   let [checkList, setCheckList] = useState([
-    { name: "Happy", checked: false },
-    { name: "NotHappy", checked: false },
+    { name: "Можно курить", checked: false },
+    { name: "Можно с питомцами", checked: false },
+    { name: "Можно пригласить гостей (до 10 человек)", checked: false },
   ]);
   function Change(index) {
     let newArr = checkList.map((item, i) => {
       if (index === i) {
-        return { ...item, checked: !item.checked };
+        return { ...item, checked: !checkList[i].checked };
       } else {
         return item;
       }
     });
     setCheckList(newArr);
   }
+
   return (
     <div className="Checkbox">
       <div>
         <div>Checkbox buttons</div>
         <div></div>
       </div>
-      <button onClick={() => console.log(checkList)}></button>
       <form action="none" className="check-list">
         {checkList.map((item, index) => {
           return (
-            <label onClick={() => Change(index)} key={item.name}>
-              <input type="checkbox" className="custom-checkbox" />
-              <span>{item.name}</span>
-              <div>{console.log(item.checked)}</div>
+            <label key={item.name}>
+              <input type="checkbox" className="checkbox" />
+              {item.checked ? (
+                <span
+                  className="checked"
+                  onClick={() => {
+                    Change(index);
+                  }}
+                >
+                  {item.name}
+                </span>
+              ) : (
+                <span
+                  onClick={() => {
+                    Change(index);
+                  }}
+                >
+                  {item.name}
+                </span>
+              )}
             </label>
           );
         })}
